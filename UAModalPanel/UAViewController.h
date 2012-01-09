464b7de7279a6a5944e51add75e6fd0294e78e69
Matt Coneybeare <coneybeare@gmail.com>
2012-01-08 22:39:39 -0500

    Initial Commit

---
+++ UAModalPanel/UAViewController.h
@@ -0,0 +1,13 @@
+//
+//  UAViewController.h
+//  UAModalPanel
+//
+//  Created by Matt Coneybeare on 1/8/12.
+//  Copyright (c) 2012 Urban Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UAViewController : UIViewController
+
+@end
